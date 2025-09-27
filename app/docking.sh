8ffc06a162d69f51040841617414a58256cda7d2
Reaper-Official <reaper@etik.com>
2025-09-27 20:02:04 +0000

    send

--- app/docking.sh
+++ app/docking.sh
@@ -1,9 +1,3 @@
-cd /workspaces/cyber-prevention-tool/app
-
-# Nettoyer complètement
 docker compose down -v
-docker rmi app-backend app-frontend 2>/dev/null || true
-
-docker compose build --no-cache frontend
 docker compose build --no-cache backend
 docker compose up -d
