ccde9d7000cf0a0bcd4b96e06bb0be2859b0f8bb
Reaper-Official <reaper@etik.com>
2025-10-01 18:55:53 +0000

    qds

--- app/inserter.sh
+++ app/inserter.sh
@@ -1,5 +1,3 @@
-# Renommez le fichier
-mv backend/prisma/training-modules.js backend/prisma/training-modules.ts
-
-# Modifiez l'import dans seed.ts
-sed -i "s/from '.\/training-modules.js'/from '.\/training-modules'/" backend/prisma/seed.ts
+docker compose restart backend
+sleep 15
+docker compose exec backend npm run prisma:seed
